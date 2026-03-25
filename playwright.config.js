84180224563b863571b67bbb407086aa34aa83f3
AnikSen <37754808+aniksen0@users.noreply.github.com>
2026-03-26 02:43:30 +0600

    added html reporter for artifact generation

--- playwright.config.js
+++ playwright.config.js
@@ -12,6 +12,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
+    ['html', { open: 'never' }],
     ['allure-playwright', { outputFolder: 'allure-results' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
